958b81e87eb7f143490137eff0b844c0fe686a39
Suqatri <paul@suqatri.net>
2022-11-22 00:20:09 +0100

    add lombok

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,13 +1,15 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java")
+    kotlin("jvm") version "1.7.21"
 }
 
 group = "com.dustrean.api"
 
 repositories {
-
+    mavenCentral()
 }
 
 dependencies {
-
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    kotlin("plugin.lombok") version "1.7.21"
+    id("io.freefair.lombok") version "5.3.0"
 }
 
 group = "com.dustrean"
 version = "1.0-SNAPSHOT"
 
 allprojects {
+
+    apply(plugin = "java")
+    apply(plugin = "io.freefair.lombok")
 
     repositories {
         mavenCentral()
@@ -17,8 +22,8 @@ allprojects {
 
     }
 
-}
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
 }
