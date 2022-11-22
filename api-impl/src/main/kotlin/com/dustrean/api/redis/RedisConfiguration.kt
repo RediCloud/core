cb4417c3a2dedc4a45c91171ede409cd118c1441
Anton <anton.wiblishauser@gmx.de>
2022-11-22 17:57:00 +0100

    remove unused comma

--- api-impl/src/main/kotlin/com/dustrean/api/redis/RedisConfiguration.kt
+++ api-impl/src/main/kotlin/com/dustrean/api/redis/RedisConfiguration.kt
@@ -4,5 +4,5 @@ data class RedisConfiguration(
     val subscriptionConnectionMinimumIdleSize: Int = 1,
     val subscriptionConnectionPoolSize: Int = 50,
     val connectionMinimumIdleSize: Int = 24,
-    val connectionPoolSize: Int = 64,
+    val connectionPoolSize: Int = 64
 )
