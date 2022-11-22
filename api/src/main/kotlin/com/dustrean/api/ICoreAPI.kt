687b32c14ed3ff1920f634082d3b6bc5a5d2fff3
Suqatri <paul@suqatri.net>
2022-11-22 16:19:52 +0100

    add INetworkComponentInfo to ICoreAPI

--- api/src/main/kotlin/com/dustrean/api/ICoreAPI.kt
+++ api/src/main/kotlin/com/dustrean/api/ICoreAPI.kt
@@ -1,9 +1,12 @@
 package com.dustrean.api
 
 import com.dustrean.api.module.IModuleManager
+import com.dustrean.api.network.INetworkComponentInfo
 import com.dustrean.api.tasks.Scheduler
 
 interface ICoreAPI {
+
+    fun getNetworkComponentInfo(): INetworkComponentInfo
 
     fun getCoreVersion(): String
 
