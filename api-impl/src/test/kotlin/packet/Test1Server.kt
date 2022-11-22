23ce66013ae4de889130db1183f6508ce5a98f9e
Suqatri <paul@suqatri.net>
2022-11-22 22:40:27 +0100

    create packet tests

---
+++ api-impl/src/test/kotlin/packet/Test1Server.kt
@@ -0,0 +1,33 @@
+import com.dustrean.api.network.NetworkComponentInfo
+import com.dustrean.api.network.NetworkComponentType
+import com.dustrean.api.packet.PacketManager
+import com.dustrean.api.redis.RedisConnection
+import com.dustrean.api.redis.RedisCredentials
+import packet.TestPacket
+import java.util.*
+
+fun main(args: Array<String>){
+
+    val networkComponentInfo1 = NetworkComponentInfo(NetworkComponentType.STANDALONE, UUID.fromString("75dfe2a8-6aa7-11ed-a1eb-0242ac120002"))
+    val networkComponentInfo2 = NetworkComponentInfo(NetworkComponentType.STANDALONE, UUID.fromString("85dfe2a8-6aa7-11ed-a1eb-0242ac120002"))
+
+    println("Network info: $networkComponentInfo1")
+
+    val credentials = RedisCredentials("168.119.60.230", 6379, "sOmE_sEcUrE_pAsS")
+    val connection = RedisConnection(credentials)
+    connection.connect()
+
+    println("Connected to redis")
+
+    val packetManager = PacketManager(networkComponentInfo1, connection)
+
+    packetManager.registerPacket(TestPacket())
+
+    val packet = TestPacket()
+    packet.message = "Hello from test1"
+    packet.packetData.receiverComponent.add(networkComponentInfo2)
+
+    packetManager.sendPacket(packet)
+
+    println("Packet sent")
+}
