0c59f5ffe9ee0a2302e7a66d4147fe5a6e044d99
Anton <anton.wiblishauser@gmx.de>
2022-11-22 17:14:36 +0100

    remove test implementations

--- api-impl/build.gradle.kts
+++ api-impl/build.gradle.kts
@@ -3,17 +3,8 @@ plugins {
 }
 
 group = "com.dustrean.api"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
