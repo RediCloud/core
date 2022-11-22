be45aa6df046bf29d569385a4347cc2aaba927cb
Suqatri <paul@suqatri.net>
2022-11-22 22:39:51 +0100

    add Serializable

--- api/src/main/kotlin/com/dustrean/api/packet/PacketData.kt
+++ api/src/main/kotlin/com/dustrean/api/packet/PacketData.kt
@@ -1,17 +1,20 @@
 package com.dustrean.api.packet
 
-import com.dustrean.api.network.INetworkComponentInfo
+import com.dustrean.api.network.NetworkComponentInfo
 import com.dustrean.api.packet.coroutines.FutureAction
 import com.dustrean.api.packet.response.PacketResponse
+import com.fasterxml.jackson.annotation.JsonIgnore
+import java.io.Serializable
 import java.util.UUID
 import kotlin.time.Duration.Companion.seconds
 
-class PacketData {
+class PacketData : Serializable{
     val packetId: UUID = UUID.randomUUID()
-    lateinit var senderComponent: INetworkComponentInfo
-    val receiverComponent: ArrayList<INetworkComponentInfo> = arrayListOf()
+    lateinit var senderComponent: NetworkComponentInfo
+    val receiverComponent: ArrayList<NetworkComponentInfo> = arrayListOf()
     var allowSenderAsReceiver: Boolean = false
     var responsePacketData: PacketData? = null
+    @JsonIgnore
     var futureResponse: FutureAction<PacketResponse>? = null
 
     fun allowSenderAsReceiver() {
@@ -20,9 +23,9 @@ class PacketData {
 
     fun isSenderAsReceiverAllowed(): Boolean = allowSenderAsReceiver
 
-    fun addReceiver(receiver: INetworkComponentInfo) = receiverComponent.add(receiver)
+    fun addReceiver(receiver: NetworkComponentInfo) = receiverComponent.add(receiver)
 
-    fun removeReceiver(receiver: INetworkComponentInfo) = receiverComponent.remove(receiver)
+    fun removeReceiver(receiver: NetworkComponentInfo) = receiverComponent.remove(receiver)
 
     fun clearReceiverComponent() = receiverComponent.clear()
 
@@ -32,5 +35,5 @@ class PacketData {
         return futureResponse!!
     }
 
-    fun hasReceiver(componentInfo: INetworkComponentInfo): Boolean = receiverComponent.contains(componentInfo)
+    fun hasReceiver(componentInfo: NetworkComponentInfo): Boolean = receiverComponent.contains(componentInfo)
 }
