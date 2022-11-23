ad694742e442aef88d4d8c6760798be858a4304e
Suqatri <paul@suqatri.net>
2022-11-23 16:21:58 +0100

    set redission default codec

--- api-impl/src/main/kotlin/com/dustrean/api/redis/RedisConnection.kt
+++ api-impl/src/main/kotlin/com/dustrean/api/redis/RedisConnection.kt
@@ -1,5 +1,7 @@
 package com.dustrean.api.redis
 
+import com.dustrean.api.redis.codec.JsonJacksonKotlinCodec
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
 import org.redisson.config.Config
@@ -15,6 +17,8 @@ class RedisConnection(
         config.useSingleServer().setConnectionPoolSize(configuration.connectionPoolSize)
             .setConnectionMinimumIdleSize(configuration.connectionMinimumIdleSize)
             .setAddress("redis://${credentials.host}:${credentials.port}").setPassword(credentials.password)
+
+        config.codec = JsonJacksonKotlinCodec(ObjectMapper())
 
         redisClient = Redisson.create(config)
     }
