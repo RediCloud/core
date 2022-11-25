f9902d1afb3cac87f3220f59ca2f515e6b1abb62
Suqatri <paul@suqatri.net>
2022-11-25 18:26:45 +0100

    module -> abstract class

--- api/src/main/kotlin/com/dustrean/api/module/IModule.kt
+++
@@ -1,9 +0,0 @@
-package com.dustrean.api.module
-
-import com.dustrean.api.CoreAPI
-
-interface IModule{
-    fun onLoad(api: CoreAPI)
-    fun onEnable(api: CoreAPI)
-    fun onDisable(api: CoreAPI)
-}
