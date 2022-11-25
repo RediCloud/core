64838bffd5ac8ecde69fa81cef421d4085b85c21
Suqatri <paul@suqatri.net>
2022-11-25 18:28:50 +0100

    add kotlinx json serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
 }
 
 group = "com.dustrean"
@@ -9,6 +10,7 @@ version = "1.0-SNAPSHOT"
 
 allprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "kotlinx-serialization")
 
     repositories {
         mavenCentral()
@@ -17,6 +19,7 @@ allprojects {
     dependencies {
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
         implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     }
 
     tasks.withType<KotlinCompile> {
