8aa5e1959b691c318a7b8cae51282ab7695cc6a0
Suqatri <paul@suqatri.net>
2022-11-26 13:36:16 +0100

    impl to CoreApiImpl

--- api-impl/src/main/kotlin/net/dustrean/api/CoreAPIImpl.kt
+++ api-impl/src/main/kotlin/net/dustrean/api/CoreAPIImpl.kt
@@ -1,3 +1,37 @@
 package net.dustrean.api
 
-abstract class CoreAPIImpl
+import net.dustrean.api.event.EventManager
+import net.dustrean.api.event.IEventManager
+import net.dustrean.api.module.IModuleManager
+import net.dustrean.api.module.ModuleManager
+import net.dustrean.api.network.NetworkComponentInfo
+import net.dustrean.api.packet.IPacketManager
+import net.dustrean.api.packet.PacketManager
+import net.dustrean.api.redis.RedisConnection
+import net.dustrean.api.utils.coreVersion
+import net.dustrean.api.utils.getModuleFolder
+
+abstract class CoreAPIImpl(
+    private val networkComponentInfo: NetworkComponentInfo
+) : CoreAPI{
+
+    private var redisConnection: RedisConnection = RedisConnection()
+    private var packetManager: PacketManager = PacketManager(networkComponentInfo, redisConnection)
+    private var eventManager: EventManager = EventManager()
+    private var moduleManager: ModuleManager = ModuleManager(this)
+
+    init {
+        moduleManager.detectModules(getModuleFolder())
+    }
+
+    override fun getNetworkComponentInfo(): NetworkComponentInfo = networkComponentInfo
+
+    override fun getPacketManager(): IPacketManager = packetManager
+
+    override fun getCoreVersion(): String = coreVersion
+
+    override fun getEventManager(): IEventManager = eventManager
+
+    override fun getModuleHandler(): IModuleManager = moduleManager
+
+}
