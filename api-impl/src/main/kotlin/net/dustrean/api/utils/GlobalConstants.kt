a3619d09f6b1aa3ad565ae607b7486b1cb65e760
Suqatri <paul@suqatri.net>
2022-11-26 13:34:20 +0100

    Create GlobalConstants.kt

---
+++ api-impl/src/main/kotlin/net/dustrean/api/utils/GlobalConstants.kt
@@ -0,0 +1,21 @@
+package net.dustrean.api.utils
+
+import java.io.File
+
+fun getManageFolder(): File {
+    val manageFolder = File(System.getenv("dustrean.manage.folder"))
+    if (!manageFolder.exists()) {
+        manageFolder.mkdir()
+    }
+    return manageFolder
+}
+
+fun getModuleFolder(): File {
+    val moduleFolder = File(getManageFolder(), "modules")
+    if (!moduleFolder.exists()) {
+        moduleFolder.mkdir()
+    }
+    return moduleFolder
+}
+
+val coreVersion = ""
