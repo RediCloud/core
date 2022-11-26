3850703d718d126e62f04a284bb89dac7ce1eac8
Suqatri <paul@suqatri.net>
2022-11-26 14:46:47 +0100

    add cloud net repo/dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,15 +14,30 @@ allprojects {
 
     repositories {
         mavenCentral()
+        gradlePluginPortal()
+        maven("https://repo.cloudnetservice.eu/repository/releases/")
     }
 
     dependencies {
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-        implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.0")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
+        implementation("com.fasterxml.jackson.core:jackson-annotations:2.14.0")
+
+        compileOnly("eu.cloudnetservice.cloudnet:driver:4.0.0-RC5")
+        compileOnly("eu.cloudnetservice.cloudnet:bridge:4.0.0-RC5")
+        compileOnly("eu.cloudnetservice.cloudnet:wrapper-jvm:4.0.0-RC5")
+        compileOnly("eu.cloudnetservice.cloudnet:cloudperms:4.0.0-RC5")
     }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "17"
+        }
+
+        withType<JavaCompile> {
+            options.release.set(17)
+            options.encoding = "UTF-8"
+        }
     }
 }
