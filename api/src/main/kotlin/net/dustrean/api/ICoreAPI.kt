7236762ad19a7da0ba01f6bef37fdfb479af5b7f
Suqatri <paul@suqatri.net>
2022-11-27 17:21:04 +0100

    refactor: remove scheduler from core api

--- api/src/main/kotlin/net/dustrean/api/ICoreAPI.kt
+++ api/src/main/kotlin/net/dustrean/api/ICoreAPI.kt
@@ -4,14 +4,11 @@ import net.dustrean.api.event.IEventManager
 import net.dustrean.api.module.IModuleManager
 import net.dustrean.api.network.NetworkComponentInfo
 import net.dustrean.api.packet.IPacketManager
-import net.dustrean.api.tasks.Scheduler
 
 interface ICoreAPI {
     fun getNetworkComponentInfo(): NetworkComponentInfo
 
     fun getCoreVersion(): String
-
-    fun getScheduler(): Scheduler<*, *>
 
     fun getModuleHandler(): IModuleManager
 
