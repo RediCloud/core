161892b36a6dcd858f5f51d242af9cc34078c68d
Suqatri <paul@suqatri.net>
2022-11-27 17:42:53 +0100

    feat: impl velocity plugin loading

---
+++ api-velocity/src/main/kotlin/net/dustrean/api/velocity/boostrap/VelocityBootstrap.kt
@@ -0,0 +1,26 @@
+package net.dustrean.api.velocity.boostrap
+
+import com.velocitypowered.api.plugin.Plugin
+import com.velocitypowered.api.proxy.ProxyServer
+import net.dustrean.api.velocity.VelocityCoreAPI
+
+@Plugin(
+    id = "core",
+    name = "core",
+    version = "1.0.0-SNAPSHOT",
+    description = "Core plugin for the Dustrean API",
+    authors = ["Dustrean-Team"]
+)
+class VelocityBootstrap(proxyServer: ProxyServer) {
+
+    val core: VelocityCoreAPI = null!!
+
+    init {
+        VelocityBootstrap::class.java.getDeclaredField("core").apply {
+            isAccessible = true
+            set(this@VelocityBootstrap, VelocityCoreAPI(proxyServer))
+            isAccessible = false
+        }
+    }
+
+}
