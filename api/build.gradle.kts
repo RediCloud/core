9361e0cd5e058ca08ae038320e9a30b4e01301e8
Anton <anton.wiblishauser@gmx.de>
2022-11-28 16:06:01 +0100

    add maven publish script

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,45 @@
 plugins {
     kotlin("jvm") version "1.7.21"
+    signing
+    `maven-publish`
 }
 
 group = "net.dustrean.api"
+val projectVersion = "1.0.0"
+version = projectVersion
+val snapshot = false
 
 repositories {
     mavenCentral()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "dustrean"
+            url = uri( if (!snapshot)"https://repo.dustrean.net/releases" else "https://repo.dustrean.net/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.dustrean"
+            artifactId = "api"
+            version = projectVersion
+            from(components["java"])
+        }
+    }
+}
+
+
+signing {
+    sign(publishing.publications)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
