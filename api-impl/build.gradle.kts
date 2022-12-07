8e9f7452a193462ab0d7ac07f2780e14cf7bd8be
SteinGaming <danialdaryab@gmail.com>
2022-12-07 20:33:43 +0100

    testing: fix file size

--- api-impl/build.gradle.kts
+++ api-impl/build.gradle.kts
@@ -3,9 +3,17 @@ plugins {
 }
 
 val projects by extra(listOf("api"))
+
+val default: Configuration by configurations.creating
+
+configurations.compileClasspath.get().extendsFrom(default)
+
+the(net.dustrean.libloader.plugin.LibraryLoader.LibraryLoaderConfig::class).apply {
+    this.configurationName.set("default")
+}
 dependencies {
     compileOnly(project(":api"))
     testImplementation(project(":api"))
-    implementation("org.redisson:redisson:3.18.0")
-    implementation("com.google.guava:guava:31.1-jre")
+    add("default","org.redisson:redisson:3.18.0")
+    add("default", "com.google.guava:guava:31.1-jre")
 }
