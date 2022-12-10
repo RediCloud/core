e24284864a5b89979f2917620f39f7afab8cdf5c
SteinGaming <danialdaryab@gmail.com>
2022-12-10 17:56:00 +0100

    feat: standalone api

---
+++ api-standalone/src/main/kotlin/net/dustrean/api/standalone/Main.kt
@@ -0,0 +1,36 @@
+package net.dustrean.api.standalone
+
+import net.dustrean.api.module.ModuleState
+import net.dustrean.api.standalone.impl.StandaloneCoreAPI
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    println("Starting API Standalone...")
+    val coreAPI = StandaloneCoreAPI()
+    println("API Standalone started!")
+    while (true) {
+        try {
+            print("\nAvailable commands: list, reload, stop\n> ")
+            val command = readlnOrNull()!!
+            when (command.lowercase()) {
+                "list" -> println(ModuleState.values().joinToString(separator = "\n") {
+                    "${it.name}:    ${coreAPI.getModuleHandler().getModules(it).joinToString()}"
+                })
+                "reload" -> {
+                    println("Reloading modules...")
+                    coreAPI.getModuleHandler().reloadModules()
+                    println("Modules reloaded!")
+                }
+                "stop" -> {
+                    println("Stopping API Standalone...")
+                    coreAPI.shutdown()
+                    println("API Standalone stopped!")
+                    exitProcess(0)
+                }
+                else -> println("Unknown command!")
+            }
+        } catch (t: Throwable) {
+            t.printStackTrace()
+        }
+    }
+}
