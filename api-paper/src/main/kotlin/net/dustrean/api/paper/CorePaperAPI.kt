7fbb5b88769212243f2fdbeeeb22c5b98902b028
ChaotenHG <chaoten@neverstopgaming.net>
2022-12-14 19:46:53 +0100

    feat: add ExceptionHandler

--- api-paper/src/main/kotlin/net/dustrean/api/paper/CorePaperAPI.kt
+++ api-paper/src/main/kotlin/net/dustrean/api/paper/CorePaperAPI.kt
@@ -2,11 +2,16 @@ package net.dustrean.api.paper
 
 import net.dustrean.api.cloud.CloudCoreAPI
 import net.dustrean.api.paper.utils.parser.PlayerParser
+import net.dustrean.api.utils.ExceptionHandler
 import net.dustrean.api.utils.parser.string.StringParser
 
 object CorePaperAPI : CloudCoreAPI() {
 
     init {
+
+        ExceptionHandler
+
         StringParser.customTypeParsers.add(PlayerParser())
+
     }
 }
