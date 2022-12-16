81af9bbb8cbb7c39d9672846d876859d310510f5
Suqatri <paul@suqatri.net>
2022-12-16 22:51:01 +0100

    Update Module.kt

--- api/src/main/kotlin/net/dustrean/api/module/Module.kt
+++ api/src/main/kotlin/net/dustrean/api/module/Module.kt
@@ -7,7 +7,7 @@ abstract class Module {
     var state = ModuleState.DISABLED
 
     // The module description will be set by the module manager
-    val description: ModuleDescription = ModuleDescription("unknown", "unknown", "unknown", emptyMap())
+    val description: ModuleDescription = ModuleDescription("unknown", "unknown", "unknown", HashMap())
 
     abstract fun onLoad(api: ICoreAPI)
     abstract fun onEnable(api: ICoreAPI)
