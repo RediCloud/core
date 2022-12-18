7f376490399544b1adb5cfacb396306dee6ff53f
SteinGaming <danialdaryab@gmail.com>
2022-12-18 17:20:27 +0100

    feat: return self as FutureAction

--- api/src/main/kotlin/net/dustrean/api/data/AbstractDataObject.kt
+++ api/src/main/kotlin/net/dustrean/api/data/AbstractDataObject.kt
@@ -1,11 +1,12 @@
 package net.dustrean.api.data
 
+import net.dustrean.api.tasks.futures.FutureAction
 import java.io.Serializable
 import java.util.*
 
 abstract class AbstractDataObject() : Serializable {
 
-    abstract fun update()
+    abstract fun update(): FutureAction<out AbstractDataObject>
 
     abstract fun getIdentifier(): UUID
 
