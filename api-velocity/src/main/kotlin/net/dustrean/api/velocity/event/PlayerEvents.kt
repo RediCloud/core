344209d68c2368746254acb31e37cc7f86730805
Suqatri <paul@suqatri.net>
2022-12-19 21:34:52 +0100

    set new ips on player proxy join

--- api-velocity/src/main/kotlin/net/dustrean/api/velocity/event/PlayerEvents.kt
+++ api-velocity/src/main/kotlin/net/dustrean/api/velocity/event/PlayerEvents.kt
@@ -8,7 +8,6 @@ import net.dustrean.api.CoreAPI
 import net.dustrean.api.ICoreAPI
 import net.dustrean.api.player.Player
 import net.dustrean.api.player.PlayerManager
-import java.util.*
 
 class PlayerEvents(private val playerManager: PlayerManager) {
     @Subscribe
@@ -27,6 +26,10 @@ class PlayerEvents(private val playerManager: PlayerManager) {
                 player.name = event.player.username
                 // TODO: PlayerNameUpdate Event CoreAPI
             }
+
+            if (player.getLastIp() != event.player.remoteAddress.address.hostAddress)
+                player.ipHistory.add(System.currentTimeMillis() to event.player.remoteAddress.address.hostAddress)
+
             player.lastProxy = ICoreAPI.getInstance<CoreAPI>().getNetworkComponentInfo()
             player.currentlyOnline = true
             player.update()
@@ -37,12 +40,13 @@ class PlayerEvents(private val playerManager: PlayerManager) {
         ).apply {
             playerManager.nameFetcher[event.player.username.lowercase()] = event.player.uniqueId
             nameHistory.add(System.currentTimeMillis() to event.player.username)
+            ipHistory.add(System.currentTimeMillis() to event.player.remoteAddress.address.hostAddress)
             lastProxy = ICoreAPI.INSTANCE.getNetworkComponentInfo()
         })
     }
 
     @Subscribe
-    fun onPlayerDisconnect(event: DisconnectEvent){
+    fun onPlayerDisconnect(event: DisconnectEvent) {
         playerManager.onlineFetcher.remove(event.player.uniqueId)
     }
 }
