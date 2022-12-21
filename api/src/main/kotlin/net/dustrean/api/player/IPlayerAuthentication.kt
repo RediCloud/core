2ae21fa0a910172296fd69d56c274c1f696c281d
Suqatri <paul@suqatri.net>
2022-12-21 21:04:46 +0100

    add crypt to player auth

--- api/src/main/kotlin/net/dustrean/api/player/IPlayerAuthentication.kt
+++ api/src/main/kotlin/net/dustrean/api/player/IPlayerAuthentication.kt
@@ -1,5 +1,8 @@
 package net.dustrean.api.player
 
+import com.google.gson.annotations.Expose
+import net.dustrean.api.utils.crypt.UpdatableBCrypt
+
 interface IPlayerAuthentication {
 
     var passwordHash: String
@@ -8,7 +11,7 @@ interface IPlayerAuthentication {
     var lastVerify: Long
     var cracked: Boolean
     val loginProcess: Boolean
-
+    var crypt: UpdatableBCrypt?
     fun isLoggedIn(player: IPlayer): Boolean
 
 }
