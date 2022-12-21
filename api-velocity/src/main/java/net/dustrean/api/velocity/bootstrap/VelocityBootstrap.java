5ab132c1e87aa945efe763655b8fadaabf02b611
Suqatri <paul@suqatri.net>
2022-12-21 18:17:54 +0100

    fix: velocity command meta

--- api-velocity/src/main/java/net/dustrean/api/velocity/bootstrap/VelocityBootstrap.java
+++ api-velocity/src/main/java/net/dustrean/api/velocity/bootstrap/VelocityBootstrap.java
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.PluginContainer;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.dustrean.api.velocity.VelocityCoreAPI;
 import net.dustrean.libloader.boot.Bootstrap;
@@ -33,7 +34,7 @@ public class VelocityBootstrap {
 
     @Subscribe
     public void onInit(ProxyInitializeEvent event) {
-        VelocityCoreAPI.INSTANCE.init(this.proxyServer);
+        VelocityCoreAPI.INSTANCE.init(this.proxyServer, (PluginContainer) this);
     }
 
     @Subscribe
