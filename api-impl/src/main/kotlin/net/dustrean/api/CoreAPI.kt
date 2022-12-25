b4ed44e2b29f105c8e4cab92e338e27db5f2c6db
Anton <anton.wiblishauser@gmx.de>
2022-12-25 16:12:57 +0100

    fix init in CoreAPI

--- api-impl/src/main/kotlin/net/dustrean/api/CoreAPI.kt
+++ api-impl/src/main/kotlin/net/dustrean/api/CoreAPI.kt
@@ -19,20 +19,19 @@ abstract class CoreAPI(
     private val networkComponentInfo: NetworkComponentInfo
 ) : ICoreAPI {
 
+    init {
+        ICoreAPI.INSTANCE = this
+    }
+
     private var redisConnection: RedisConnection = RedisConnection()
     private var packetManager: PacketManager = PacketManager(networkComponentInfo, redisConnection)
     private var eventManager: EventManager = EventManager()
-    private var networkComponentManager: NetworkComponentManager = NetworkComponentManager(redisConnection)
+    private var networkComponentManager: NetworkComponentManager = NetworkComponentManager(redisConnection).also {
+        it.networkComponents[networkComponentInfo.getKey()] = networkComponentInfo
+    }
     private var configManager: ConfigManager = ConfigManager(redisConnection)
     private var playerManager: PlayerManager = PlayerManager(this)
-    private var moduleManager: ModuleManager = ModuleManager(this)
-
-    init {
-        ICoreAPI.INSTANCE = this
-        networkComponentManager.networkComponents[networkComponentInfo.getKey()] = networkComponentInfo
-
-        moduleManager.enableModules()
-    }
+    private var moduleManager: ModuleManager = ModuleManager(this).also { it.enableModules() }
 
     override fun shutdown() {
         moduleManager.disableModules()
