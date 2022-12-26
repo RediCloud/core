6eaa272ed98747e9a1ec78ea0616efa10240f5bd
Anton <anton.wiblishauser@gmx.de>
2022-12-26 15:23:04 +0100

    init ExceptionHandler on every service
    Co-Authored-By: Suqatri <44299323+Suqatri@users.noreply.github.com>

--- api-standalone/src/main/kotlin/net/dustrean/api/standalone/impl/StandaloneCoreAPI.kt
+++ api-standalone/src/main/kotlin/net/dustrean/api/standalone/impl/StandaloneCoreAPI.kt
@@ -4,9 +4,14 @@ import net.dustrean.api.CoreAPI
 import net.dustrean.api.network.NetworkComponentInfo
 import net.dustrean.api.network.NetworkComponentType
 import net.dustrean.api.standalone.commands.StandaloneCommandManager
+import net.dustrean.api.utils.ExceptionHandler
 import java.util.*
 
 object StandaloneCoreAPI : CoreAPI(NetworkComponentInfo(NetworkComponentType.STANDALONE, UUID.randomUUID())) {
+
+    init {
+        ExceptionHandler.service = "StandaloneCoreAPI"
+    }
 
     override fun getCommandManager() = StandaloneCommandManager
 
