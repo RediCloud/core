833995fbffdb681d58149b2ededfe66f93e2a296
SteinGaming <danialdaryab@gmail.com>
2022-12-28 00:32:07 +0100

    testing: removal of child, caused stackoverflow

--- api-minestom/src/main/kotlin/net/dustrean/api/minestom/bootstrap/MinestomBootstrap.kt
+++ api-minestom/src/main/kotlin/net/dustrean/api/minestom/bootstrap/MinestomBootstrap.kt
@@ -8,17 +8,15 @@ import net.dustrean.libloader.boot.Bootstrap
 import net.minestom.server.extensions.DiscoveredExtension
 import net.minestom.server.extensions.Extension
 import net.minestom.server.extensions.ExtensionClassLoader
+import java.net.URL
 
 class MinestomBootstrap : Extension() {
+    lateinit var classloader: ExtensionClassLoader
     override fun preInitialize() {
         val classloader = this.javaClass.superclass.getDeclaredMethod(
             "getExtensionClassLoader"
         ).apply { isAccessible = true }.invoke(this) as ExtensionClassLoader
-        classloader.addChild(
-            ExtensionClassLoader(
-                "Fake", arrayOf(), CoreAPI::class.java.classLoader, DiscoveredExtension()
-            )
-        )
+        this.classloader = classloader
         Bootstrap().apply({
             classloader.addURL(it)
         }, classloader, classloader)
