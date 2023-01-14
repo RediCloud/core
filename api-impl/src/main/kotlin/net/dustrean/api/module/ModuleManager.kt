7944f6b5a34360ea7eacfb7ff901b3755ee9cfee
Suqatri <paul@suqatri.net>
2023-01-14 12:22:25 +0100

    debug error handling

--- api-impl/src/main/kotlin/net/dustrean/api/module/ModuleManager.kt
+++ api-impl/src/main/kotlin/net/dustrean/api/module/ModuleManager.kt
@@ -1,6 +1,7 @@
 package net.dustrean.api.module
 
 import com.google.gson.Gson
+import net.dustrean.api.CoreAPI
 import net.dustrean.api.ICoreAPI
 import net.dustrean.api.utils.getModuleFolder
 import net.dustrean.libloader.boot.Bootstrap
@@ -80,6 +81,8 @@ class ModuleManager(
         try {
             module.onLoad(api)
         } catch (e: Exception) {
+            print("Failed to load module ${description.name}!")
+            e.printStackTrace()
             logger.error("Failed to load module ${description.name}", e)
             return false
         }
