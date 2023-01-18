e0651d7606a10f8919183869864ffbe6ec1c65b6
Suqatri <paul@suqatri.net>
2023-01-18 20:44:08 +0100

    impl json object data to RLists

--- api/src/main/kotlin/net/dustrean/api/utils/extension/RedissionCodecExtension.kt
+++ api/src/main/kotlin/net/dustrean/api/utils/extension/RedissionCodecExtension.kt
@@ -1,6 +1,8 @@
 package net.dustrean.api.utils.extension
 
 import com.google.gson.Gson
+import net.dustrean.api.ICoreAPI
+import org.redisson.api.RList
 
 
 val gson = Gson()
@@ -9,4 +11,33 @@ data class JsonObjectData(val json: String, val clazz: String)
 
 fun <T> JsonObjectData.toObject(): T = gson.fromJson(json, Class.forName(clazz)) as T
 
-inline fun <reified T> T.toJsonObjectData(): JsonObjectData = JsonObjectData(gson.toJson(this), T::class.java.name)
+fun Any.toJsonObjectData(): JsonObjectData = JsonObjectData(gson.toJson(this), this::class.java.name)
+
+fun <R, V> RList<V>.useExternal(src: R): ExternalRList<R> =
+    ExternalRList<R>(ICoreAPI.INSTANCE.getRedisConnection().getRedissonClient().getList(name))
+
+class ExternalRList<V>(private val sourceList: RList<JsonObjectData>) {
+
+    fun readAll(): List<V> = sourceList.readAll().map { it.toObject() }
+
+    fun add(element: V) = sourceList.add(element!!.toJsonObjectData())
+
+    fun remove(element: V) = sourceList.remove(element!!.toJsonObjectData())
+
+    fun contains(element: V) = sourceList.contains(element!!.toJsonObjectData())
+
+    fun size() = sourceList.size
+
+    fun isEmpty() = sourceList.isEmpty()
+
+    fun addAll(elements: Collection<V>) = sourceList.addAll(elements.map { it!!.toJsonObjectData() })
+
+    fun removeAll(elements: Collection<V>) = sourceList.removeAll(elements.map { it!!.toJsonObjectData() })
+
+    fun clear() = sourceList.clear()
+
+    fun get(index: Int): V = sourceList.get(index).toObject()
+
+    fun removeIf(filter: (V) -> Boolean) = sourceList.removeIf { filter(it.toObject()) }
+
+}
