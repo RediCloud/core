415ee56a7cdef087d6093931cc917007ec96776e
Suqatri <paul@suqatri.net>
2023-01-18 20:59:13 +0100

    fix: building

--- api-impl/src/main/kotlin/net/dustrean/api/config/ConfigUpdatePacket.kt
+++ api-impl/src/main/kotlin/net/dustrean/api/config/ConfigUpdatePacket.kt
@@ -3,11 +3,12 @@ package net.dustrean.api.config
 import net.dustrean.api.CoreAPI
 import net.dustrean.api.ICoreAPI
 import net.dustrean.api.packet.Packet
+import net.dustrean.api.utils.extension.JsonObjectData
 
 class ConfigUpdatePacket : Packet() {
 
     lateinit var key: String
-    lateinit var configData: ConfigData
+    lateinit var configData: JsonObjectData
     var delete = false
 
     override fun received() {
